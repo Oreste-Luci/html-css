b52caacba510f931468f7ca9230aeb7598e9821c
Oreste Luci <oreste@luci.cl>
2015-12-25 10:28:01 +0100

    Add progress bar functionality.

--- video-player/player.js
+++ video-player/player.js
@@ -1,7 +1,12 @@
 window.addEventListener('load', function() {
 
+    // Video Element
     video = document.getElementById('video');
     
+    // Progress Bar Element
+    pbar = document.getElementById('pbar');
+    
+    // Play Button Element
     playButton = document.getElementById('play-button');
 
     video.load(); // Making sure the video loads before it can be played
@@ -18,8 +23,21 @@ function playOrPause() {
     if (video.paused) {
         video.play();
         playButton.src = 'images/pause.png';
+        update = setInterval(updatePlayer, 30); // Calling the updatePlayer function every 30ms
     } else {
         video.pause();
         playButton.src = 'images/play.png';
+        window.clearInterval(update);
+    }
+}
+
+function updatePlayer() {
+
+    var percentage = (video.currentTime/video.duration) * 100;
+    pbar.style.width = percentage + '%';
+    
+    if (video.ended) {
+        window.clearInterval(update);
+        playButton.src = 'images/replay.png';
     }
 }
